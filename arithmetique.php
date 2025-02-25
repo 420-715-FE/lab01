eff88abae6a3a054346e7e00e94f5aea3f8991f7
Pier-Luc Brault <pier-luc@brault.me>
2025-02-24 23:28:45 -0500

    Retrait de la solution

--- arithmetique.php
+++ arithmetique.php
@@ -1,27 +1,14 @@
-<?php
-
-$n1 = $_GET['n1'];
-$n2 = $_GET['n2'];
-$n3 = $n1 + $n2;
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Retour</title>
+    <title>Laboratoire 01</title>
     <link rel="stylesheet" href="water.css">
 </head>
 <body>
     <nav>
         <a href="index.html">Retour</a>
     </nav>
-    <h1>Fibonacci</h1>
-    <p>
-        <?= $n3; ?>
-        <a href="fibonacci.php?n1=<?= $n2; ?>&n2=<?= $n3; ?>">&gt;</a>
-    </p>
 </body>
 </html>
